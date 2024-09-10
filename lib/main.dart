666061f3f9f40d133f31fb89349021ae77bae0c8
Muhameed06 <muhamed06v@gmail.com>
2024-09-11 01:20:23 +0200

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -13,20 +14,35 @@ void main() {
   ));
 }
 
-const url = 'https://picsum.photos/250?image=9';
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((value) => value >= 0)
+        .listen((event) {
+      value = event;
+    });
+  }
+
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
+
+class Test {
+  Test();
+}
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(() => 
-        NetworkAssetBundle(Uri.parse(url))
-            .load(url)
-            .then((data) => data.buffer.asUint8List())
-            .then((data) => Image.memory(data)));
-
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
 
     return Scaffold(
       appBar: AppBar(
@@ -36,9 +52,7 @@ class HomePage extends HookWidget {
           'Home page',
         ),
       ),
-      body: Column(
-        children: [snapshot.hasData ? snapshot.data! : null].compactMap().toList(),
-      ),
+      body: Center(child: Text(notifier.value.toString())),
     );
   }
 }
